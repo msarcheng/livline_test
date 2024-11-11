7f7dc73b555f9f1a5800981ffd6da99213465e63
Marvin Abante <abante.marvin@yahoo.com>
2024-11-12 00:36:53 +0800

    [modified] - Created index() returning all tasks with or without search supplied also limit could be use.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
+use App\Http\Services\TaskInterface;
+use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class TaskController extends Controller
+class TaskController extends BaseController
 {
-    //
+    public function __construct(private TaskInterface $taskInterface)
+    {
+        //
+    }
+
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            $requests = $request->validate([
+                'limit' => 'integer|min:1|max:100',
+                'search' => 'string|max:100|nullable'
+            ]);
+
+            $requests = [
+                'limit' => $request->input('limit', 10),
+                'search' => $request->input('search', '')
+            ];
+
+            $tasks = $this->taskInterface->index($requests);
+
+            return $this->sendResponse($tasks);
+
+        } catch (Exception $ex) {
+            return $this->sendError(
+                $ex->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    public function show(Request $request): JsonResponse
+    {
+        return request()->json([]);
+    }
+
+    public function store(TaskRequest $request): JsonResponse
+    {
+        return request()->json([]);
+    }
+
+    public function update(TaskRequest $request): JsonResponse
+    {
+        return request()->json([]);
+    }
+
+    public function destroy(Request $request): JsonResponse
+    {
+        return request()->json([]);
+    }
+
 }
