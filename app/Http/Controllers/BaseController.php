fa7a559ecf7e4a39b97e9026bdd3b7aba4d8d96d
Marvin Abante <abante.marvin@yahoo.com>
2024-11-12 00:04:59 +0800

    [new] - create BaseController to allow controllers to use same format of success and error response as JsonResponse

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @param array $result
+     * @param int $http_status
+     *
+     * @return JsonResponse
+     */
+    public function sendResponse(array $result, int $http_status = 200): JsonResponse
+    {
+        $response = [
+            'status_code' => $http_status,
+            'data' => $result
+        ];
+
+        return response()->json($response);
+    }
+
+    /**
+     * return error response.
+     *
+     * @param string $message
+     * @param int $http_status
+     * @param array $errorMessages
+     *
+     * @return JsonResponse
+     */
+    public function sendError(
+        string $message,
+        int $http_status = 404,
+        array $errorMessages = []
+    ): JsonResponse {
+
+        $response = [
+            'status_code' => $http_status,
+            'message' => $message
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $http_status);
+    }
+}
