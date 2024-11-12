508ec04cab317882964fa25595e4b668e337bc63
Marvin Abante <abante.marvin@yahoo.com>
2024-11-12 09:21:54 +0800

    [modified] - Added Task endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('auth:sanctum')->group(function() {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::controller(TaskController::class)->group(function() {
+        Route::get('/tasks', 'index')->name('tasks');
+        Route::get('/tasks/{id}', 'show')->name('show.task');
+        Route::post('/tasks', 'store')->name('store.task');
+        Route::put('/tasks/{id}', 'update')->name('update.task');
+        Route::delete('/tasks/{id}', 'delete')->name('delete.task');
+    });
 });
