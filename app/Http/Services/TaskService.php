aba21429499abde052e8845763678549a745646a
Marvin Abante <abante.marvin@yahoo.com>
2024-11-12 08:57:07 +0800

    [modified] - added index() accepting both search and limit

--- app/Http/Services/TaskService.php
+++ app/Http/Services/TaskService.php
@@ -3,12 +3,30 @@
 namespace App\Http\Services;
 
 use App\Http\Services\TaskInterface;
+use App\Models\Task;
 
 class TaskService implements TaskInterface {
 
+    public function __construct(private Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show all tasks or with search
+     *
+     * @param array $tasks
+     *
+     * @return array
+     */
     public function index(array $tasks): array
     {
-        return [];
+        $limit = $tasks['limit'];
+        $search = '%'.$tasks['search'].'%';
+
+        return $this->task::where('title', 'LIKE', $search)
+            ->whereNull('deleted_at')
+            ->paginate($limit)->toArray();
     }
 
     public function show(int $task_id): array
